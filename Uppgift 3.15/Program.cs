d8b68dedee0515ec93e732bd628e87b8ddfe875e
AdamAkhooo <adamjamal0505@gmail.com>
2022-09-30 10:32:58 +0200

    Add project files.

---
+++ Uppgift 3.15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar tänker du hyra bilen i?");
+            Console.Write("Svara här: "); int timmar = int.Parse(Console.ReadLine());
+
+            if (timmar > 11)
+            {
+                Console.WriteLine("Du får tyvärr inte hyra bilen för mer än 11 timmar");
+            }
+
+            if (timmar < 12)
+            {
+                Console.WriteLine("Det kommer kosta dig " + timmar*80 + " kronor");
+            }
+        }
+    }
+}
